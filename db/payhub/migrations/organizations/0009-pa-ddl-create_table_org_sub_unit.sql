94157f0a704982e72e3dc7317e942ff3d8a5459c
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-06-08 17:38:15 +0200

    P4ADEV-4727-create-table-org-sub-unit: fixed field names

--- db/payhub/migrations/organizations/0009-pa-ddl-create_table_org_sub_unit.sql
+++ db/payhub/migrations/organizations/0009-pa-ddl-create_table_org_sub_unit.sql
@@ -5,13 +5,13 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS organizations.org_sub_unit (
 	organization_id bigint NOT NULL,
-	org_sub_code varchar(60) NOT NULL,
-	org_sub_type varchar(30) NOT NULL,
+	sub_unit_code varchar(60) NOT NULL,
+    sub_unit_type varchar(30) NOT NULL,
     creation_date timestamp with time zone NOT NULL default now(),
     update_date timestamp with time zone NOT NULL default now(),
     update_operator_external_id text NOT NULL,
 	update_trace_id text NOT NULL default '-',
-	constraint org_sub_unit_pkey PRIMARY KEY (organization_id, org_sub_code),
+	constraint org_sub_unit_pkey PRIMARY KEY (organization_id, sub_unit_code),
 	CONSTRAINT org_sub_unit_org_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations.organization(organization_id),
 );
 
