57d0a0b5f5a4df437a67b3180c8ade9f5b3925c6
LeonardoPierangeliniNttData <leonardo.pierangelini@nttdata.com>
2026-06-09 14:31:00 +0200

    feat: P4ADEV-4732 create table broker_keys (#9)

---
+++ db/payhub/migrations/organizations/0011-pa-ddl-create_table_broker_keys.sql
@@ -0,0 +1,20 @@
+-- start transaction
+BEGIN;
+
+------------------- business logic -------------------
+CREATE TABLE IF NOT EXISTS organizations.broker_keys (
+    broker_key_id varchar(255) NOT NULL,
+    broker_id bigint NOT NULL,
+    key_type varchar(60) NOT NULL,
+    key_cipher bytea NOT NULL,
+    creation_date timestamp with time zone NOT NULL default now(),
+    update_date timestamp with time zone NOT NULL default now(),
+    update_operator_external_id text NOT NULL,
+    update_trace_id text NOT NULL default '-',
+    CONSTRAINT broker_keys_pkey PRIMARY KEY (broker_key_id),
+    CONSTRAINT broker_keys_ukey UNIQUE (broker_id, key_type),
+    CONSTRAINT broker_keys_broker_id_fkey FOREIGN KEY (broker_id) REFERENCES organizations.broker(broker_id)
+);
+
+-- final commit
+COMMIT;
