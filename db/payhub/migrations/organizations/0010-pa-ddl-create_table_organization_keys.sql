e3bf646129c1953223f826e8b5e0bd3ca9c6070e
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-06-09 09:25:07 +0200

    P4ADEV-4731-create-table-organization-keys: added table organization_keys

---
+++ db/payhub/migrations/organizations/0010-pa-ddl-create_table_organization_keys.sql
@@ -0,0 +1,21 @@
+-- start transaction
+BEGIN;
+
+------------------- business logic -------------------
+
+CREATE TABLE IF NOT EXISTS organizations.organization_keys (
+    organization_id bigint NOT NULL,
+    sub_unit_code varchar(60) NULL,
+    key_type varchar(60) NOT NULL,
+	key_cipher bytea NOT NULL,
+	creation_date timestamp with time zone NOT NULL default now(),
+	update_date timestamp with time zone NOT NULL default now(),
+	update_operator_external_id text NOT NULL,
+	update_trace_id text NOT NULL default '-',
+    constraint organization_keys_pkey PRIMARY KEY (organization_id, sub_unit_code, key_type),
+    CONSTRAINT organization_keys_org_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations.organization(organization_id),
+    CONSTRAINT organization_keys_org_sub_unit_fkey FOREIGN KEY (organization_id, sub_unit_code) REFERENCES organizations.org_sub_unit(organization_id, sub_unit_code)
+);
+
+-- final commit
+COMMIT;
