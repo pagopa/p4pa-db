369ea6c66fcf817798af34e7e876ef1f8d305c9a
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2026-06-23 14:37:15 +0200

    fix: P4ADEV-4775 add table org_sub_unit_operators (#12)

---
+++ db/payhub/migrations/organizations/0014-pa-ddl-create_table_org_sub_unit_operators.sql
@@ -0,0 +1,28 @@
+-- start transaction
+BEGIN;
+
+------------------- business logic -------------------
+CREATE SEQUENCE IF NOT EXISTS organizations.org_sub_unit_operators_seq
+    AS bigint
+    START WITH 1
+    INCREMENT BY 1 ;
+
+CREATE TABLE IF NOT EXISTS organizations.org_sub_unit_operators (
+    org_sub_unit_operator_id bigint NOT NULL default nextval('organizations.org_sub_unit_operators_seq'),
+    operator_external_user_id varchar(256) NOT NULL,
+    organization_id bigint NOT NULL,
+    sub_unit_code varchar(60) NOT NULL,
+    creation_date timestamp with time zone NOT NULL default now(),
+    update_date timestamp with time zone NOT NULL default now(),
+    update_operator_external_id text NOT NULL,
+    update_trace_id text NOT NULL default '-',
+    constraint org_sub_unit_operator_pkey PRIMARY KEY (org_sub_unit_operator_id),
+    constraint org_sub_unit_org_id_and_code_fkey
+        FOREIGN KEY (organization_id, sub_unit_code)
+        REFERENCES organizations.org_sub_unit (organization_id, sub_unit_code)
+);
+
+CREATE unique INDEX IF NOT EXISTS idx_org_sub_unit_operators_org_id_sub_code_ext_user_id ON organizations.org_sub_unit_operators (organization_id, sub_unit_code, operator_external_user_id);
+
+-- final commit
+COMMIT;
