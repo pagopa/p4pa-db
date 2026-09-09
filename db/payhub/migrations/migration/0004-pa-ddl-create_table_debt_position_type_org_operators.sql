70e6744d87b978d310a6eacf870ffff99c65dcab
mscarsel <marco.scarsella@pagopa.it>
2026-09-09 11:31:53 +0200

    [P4ADEV-4846] add consumption_date_time and index to migration.debt_position_type_org_operators

--- db/payhub/migrations/migration/0004-pa-ddl-create_table_debt_position_type_org_operators.sql
+++ db/payhub/migrations/migration/0004-pa-ddl-create_table_debt_position_type_org_operators.sql
@@ -11,12 +11,15 @@ CREATE TABLE IF NOT EXISTS migration.debt_position_type_org_operators (
     cf_operator_hash bytea NOT NULL,
     debt_position_type_org_id bigint NOT NULL,
     debt_position_type_org_code varchar(255) NOT NULL,
+    consumption_date_time timestamp with time zone,
     creation_date timestamp with time zone NOT NULL default now(),
     update_date timestamp with time zone NOT NULL default now(),
     update_operator_external_id text NOT NULL,
     update_trace_id text NOT NULL default '-',
     CONSTRAINT debt_position_type_org_operators_pkey PRIMARY KEY (debt_position_type_org_operator_id)
 );
+
+CREATE INDEX IF NOT EXISTS idx_debt_position_type_org_operators_organization_id_fiscal_code_hash ON migration.debt_position_type_org_operators (organization_id, cf_operator_hash);
 
 -- final commit
 COMMIT;
